fce72d26f591412fc6ef41dd3e911f5574c107ab
Vitória <42255482+vit090@users.noreply.github.com>
2018-08-15 15:22:38 -0300

    Lista2Ex1

---
+++ Guerreiro.cpp
@@ -0,0 +1,39 @@
+#include "Guerreiro.h"
+#include<iostream>
+#include<string>
+
+Guerreiro::Guerreiro()
+{
+	nome = "Huger";
+	idade = 40;
+	reino = "Nexos";
+}
+
+
+Guerreiro::~Guerreiro()
+{
+}
+
+void Guerreiro::andar()
+{
+	cout << "Pela estrada fora eu vou bem sozinho.\n";
+}
+
+void Guerreiro::falar()
+{
+	cout << "Eu sou " << nome << " tenho " << idade << " anos e venho do reino de " << reino<<endl;
+}
+
+void Guerreiro::atacar()
+{
+	cout << "Eu ataquei voce seu patife\n";
+}
+
+void Guerreiro::defender()
+{
+	cout << "Voce nao pode contra as minhas defesas\n";
+}
+
+void Guerreiro::pular()
+{
+}
---
+++ Guerreiro.h
@@ -0,0 +1,23 @@
+#pragma once
+#include<iostream>
+
+using namespace std;
+
+class Guerreiro
+{
+public:
+	Guerreiro();
+	~Guerreiro();
+	void andar();
+	void falar();
+	void atacar();
+	void defender();
+	void pular();
+
+private:
+	string nome, reino;
+	int idade, forca, vida;
+	
+
+};
+

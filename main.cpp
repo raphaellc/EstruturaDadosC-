43c8337c50cdfebc6f3323e48c6663c3e76f6229
NeedMoreRamos <42255688+NeedMoreRamos@users.noreply.github.com>
2018-08-16 15:22:37 -0300

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include "warrior.h"
+#include <string>
+
+
+	int main()
+	{
+		warrior Korne;
+		Korne.run();
+		Korne.strike();
+		Korne.whirl();
+		Korne.endure();
+		Korne.block();
+
+		return 0;
+	}

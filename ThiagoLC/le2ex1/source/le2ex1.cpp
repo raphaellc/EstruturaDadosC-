9f9d976e1a832299e697ab3192193cd3d8a11605
ThiagoLC29 <42255509+ThiagoLC29@users.noreply.github.com>
2018-08-16 19:28:47 -0300

    Add files via upload

---
+++ ThiagoLC/le2ex1/source/le2ex1.cpp
@@ -0,0 +1,18 @@
+// le2ex1
+
+#include "stdafx.h"
+#include "guerreiro.h"
+
+int main()
+{
+	guerreiro vrau;
+	vrau.atacar();
+	vrau.pular();
+	vrau.correr();
+	vrau.defender();
+	vrau.saquear();
+
+	system("pause");
+    return 0;
+}
+

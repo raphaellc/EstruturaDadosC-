e83641b103a7c5868872c4dfc38f84dd0088b634
Rodrigo Zwetsch <42255492+Guigodead@users.noreply.github.com>
2018-08-16 22:28:09 -0300

    Add files via upload

---
+++ RodrigoZwetsch_Lis2_Ex2/Barbaro.h
@@ -0,0 +1,13 @@
+#pragma once
+#include "Guerreiro.h"
+class Barbaro :
+	public Guerreiro
+{
+public:
+	Barbaro();
+	~Barbaro();
+
+	void Cobrolhada();
+	void Furia();
+};
+
---
+++ RodrigoZwetsch_Lis2_Ex2/Ladino.h
@@ -0,0 +1,13 @@
+#pragma once
+#include "Guerreiro.h"
+class Ladino :
+	public Guerreiro
+{
+public:
+	Ladino();
+	~Ladino();
+
+	void Apunhalada_das_sombras();
+	void Roubar();
+};
+

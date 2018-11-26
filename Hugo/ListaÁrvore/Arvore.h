4029373b0001fb9a16416fad375c17b1ca80b5f0
Huguloso <42255527+Huguloso@users.noreply.github.com>
2018-11-26 13:26:24 -0200

    ListaÁrvore

---
+++ Hugo/ListaÁrvore/Arvore.h
@@ -0,0 +1,31 @@
+#pragma once
+
+struct No {
+	int valor;
+	No* NoEsq, *NoDir;
+};
+
+class Arvore
+{
+public:
+	Arvore();
+	~Arvore();
+	void inserir(int i);
+	bool pesquisar(int v);
+	bool remover(int v);
+	No* percorrerDireita(No* r);
+	No* retornarRaiz(int v);
+	//bool podar(int v);
+
+protected:
+	No* raiz, *novo, * nodo;
+	int nElementos;
+
+	void inserir(No* r, No * n);
+	bool pesquisar(No* r, int v);
+	bool remover(No* r, int v);
+	No* retornarRaiz(No* r, int v);
+	//bool podar(No*r, No* lock, int v);
+
+}
+;

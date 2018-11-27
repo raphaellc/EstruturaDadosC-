0cbe0ad50a7f7bf5e95bba68c218b31b50150f57
Rodrigo Zwetsch <42255492+Guigodead@users.noreply.github.com>
2018-11-27 17:51:52 -0200

    Add files via upload

---
+++ RodrigoZwetsch/RodrigoZwetsch_ListaArvore_Arrumado/Arvore.h
@@ -0,0 +1,26 @@
+#pragma once
+
+struct No {
+
+	int valor;
+	No* No_da_esq, *No_da_dir;
+
+};
+class Arvore
+{
+public:
+	Arvore();
+	~Arvore();
+	void inserir(int i);
+	bool pesquisar(int v);
+	bool remover(int v);
+	No* percorrerDireita(No* r);
+	No* retornarRaiz(int v);
+protected:
+	No* raiz, *novo, *nodo;
+	int nElementos;
+	void inserir(No* r, No * n);
+	bool pesquisar(No* r, int v);
+	bool remover(No* r, int v);
+	No* retornarRaiz(No* r, int v);
+};
